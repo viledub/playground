2fa3c9778d84db94a4dd731f940f9fa5869b4be4
viledub <watercourseie@gmail.com>
2024-09-16 03:29:45 +0100

    feat(ui) task tree basic logic

---
+++ src/lib/tree/TaskTree.test.ts
@@ -0,0 +1,158 @@
+import { expect, test } from "vitest"
+
+import { CollectionState, Take, TaskTreeIterator } from "./TaskTree"
+import type { Task, TaskMap, TaskStore } from "./TaskTree"
+
+/**
+ * 1
+ *   2
+ *     3
+ *     11
+ *   4
+ *     8
+ *       9
+ *   10
+ * 5
+ *   6
+ *     7
+ */
+
+  const taskMap: TaskMap = {
+    1: {id: 1, children: [2, 4, 10], dependencies: [], priority: 100, take: Take.ONE},
+    2: {id: 2, children: [3,11], dependencies: [], priority: 5, take: Take.ONE},
+    3: {id: 3, children: [], dependencies: [10], priority: 1},
+    4: {id: 4, children: [8], dependencies: [], priority: 1},
+    5: {id: 5, children: [6], dependencies: [], priority: 1},
+    6: {id: 6, children: [7], dependencies: [], priority: 1},
+    7: {id: 7, children: [], dependencies: [], priority: 1},
+    8: {id: 8, children: [9], dependencies: [], priority: 1},
+    9: {id: 9, children: [], dependencies: [], priority: 1},
+    10: {id: 10, children: [], dependencies: [], priority: 8},
+    11: {id: 11, children: [], dependencies: [], priority: 1}
+}
+const taskStore = {
+	topLevel: [1,5], 
+	nodes: taskMap
+}
+
+function prSt(state: CollectionState){
+    switch (state) {
+        case CollectionState.EMPTY:
+            return "Empty";
+            break;
+        case CollectionState.COMPLETE:
+            return "Complete";
+            break;
+        case CollectionState.ITERATION_COMPLETE:
+            return "ITERATION_COMPLETE" 
+            break;
+        case CollectionState.OPEN:
+            return "OPEN" 
+            break;
+        case CollectionState.FOUND:
+            return "FOUND" 
+            break;
+        case CollectionState.ITERATION_CHILD_DONE:
+            return "FOUND" 
+            break;
+        default:
+            throw new Error(`Unhandled state: ${state}`);
+    }
+}
+
+const dig = () => {
+    const t = new TaskTreeIterator(taskStore);
+    const foundItems: number[] = [];
+
+    const checkFilters = (providedFilter: (queryItem: Task)=>boolean, parentId = 0) => {
+		// const items = parentId === 0 ? topLevelIds : itemMap[parentId].children;
+        const items = t.readChildren(parentId)
+		let somethingPassed = false;
+		items.map(id => t.readItem(id)).forEach((item) => {
+			const aDescendentPassed = checkFilters(providedFilter, item.id)
+			const currentItemPassed = providedFilter(item);
+			t.setFiltered(item.id, !currentItemPassed) ;
+			if(currentItemPassed || aDescendentPassed) {
+				somethingPassed = true;
+			}
+		})
+		return somethingPassed; // false when no unfiltered descendent exists
+	}
+
+
+    const worker = (parentId = 0) => {
+        let item = t.nextChild(parentId)
+        // console.log('next child', parentId, item)
+        if(!item){
+            const eval_status = t.evaluateItemStatus(parentId);
+            if (eval_status === CollectionState.ITERATION_CHILD_DONE) {
+                t.resetIterationChildDoneCounts(parentId);
+                t.resetIterationDoneCounts(parentId);
+                item = t.nextChild(parentId);
+            } else if (eval_status === CollectionState.ITERATION_COMPLETE) {
+                t.resetIterationDoneCounts(parentId);
+                item = t.nextChild(parentId);
+            } else {
+                return eval_status;
+            }
+        }
+		const status = worker(item);
+        
+        
+        console.log(item, prSt(status))
+
+		if(status === CollectionState.EMPTY) {
+            t.setIterationDone(item);
+            foundItems.push(item)
+            return CollectionState.FOUND;
+		}
+		if(status === CollectionState.FOUND) {
+            console.log(item, 'over take?', t.overTakeLimit(item))
+            if(t.overTakeLimit(item)) {
+                t.setIterationChildDone(item);
+                return CollectionState.ITERATION_CHILD_DONE
+            } else {
+                return CollectionState.OPEN
+            }
+		}
+		if(status === CollectionState.ITERATION_CHILD_DONE) {
+            if(t.overTakeLimit(item)) {
+                t.setIterationChildDone(item);
+                return CollectionState.ITERATION_CHILD_DONE
+            } else {
+                return CollectionState.OPEN
+            }
+		}
+        return CollectionState.OPEN;
+	}
+    // checkFilters((queryItem: Task) => queryItem.id!==100)
+    worker()
+    worker()
+    worker()
+    worker()
+    worker()
+    worker()
+    worker()
+    worker()
+    worker()
+    worker()
+    worker()
+    
+   
+    
+    return foundItems;
+}
+
+
+test('test', ()=>{
+    const taskTreeIterator = new TaskTreeIterator(taskStore);
+    expect(taskTreeIterator.nextChild()).toBe(1);
+    taskTreeIterator.visit(1);
+    expect(taskTreeIterator.nextChild(1)).toBe(2);
+    expect(taskTreeIterator.nextChild(2)).toBe(3);
+})
+
+test('test', ()=>{
+    const d = dig()
+    expect(d).toEqual([3, 7, 9, 7, 10, 7, 11, 7, 9, 7, 10]);
+})
---
+++ src/lib/tree/TaskTree.ts
@@ -0,0 +1,304 @@
+export type Task = {
+    id: number,
+    children: number[],
+    dependencies: number[],
+    priority: number,
+    take?: Take
+}
+export type TaskStore = {
+    topLevel: number[],
+    nodes: TaskMap,
+}
+export interface TaskMap {
+[key: number]: Task;
+}
+
+export enum Take {
+    ONE,ALL,PROGRESS
+}
+
+
+/**
+ * OPEN: Worth visiting, has viable children or viable itself
+ * FILTERED: Never viable
+ * DONE: Children all DONE or FILTERED and has been yielded itself - never viable
+ * ITERATION_DONE: Chilren are ITERATION_COMPLETE or EMPTY and it has yielded
+ * 
+ * Secondary states based on these 4 primary states in collections:
+ * EMPTY: No items in collection
+ * OPEN: Something in the list is viable
+ * COMPLETE: All items DONE and nothing ITERATION_DONE
+ * ITERATION_COMPLETE: Nothing OPEN and at least one in ITERATION_DONE 
+ */
+export enum VisitState {
+    OPEN, 
+    DONE, 
+    ITERATION_DONE,
+    ITERATION_CHILD_DONE,
+}
+
+export enum CollectionState {
+    EMPTY,
+    COMPLETE,
+    ITERATION_COMPLETE,
+    ITERATION_CHILD_DONE,
+    OPEN,
+    FOUND,
+}
+
+type CacheEntry = {
+    iterator: (treeIterator: TaskTreeIterator, items: number[]) => number,
+    visitState: VisitState,
+    visits: number,
+    filtered: boolean,
+}
+
+const simpleOrder = (treeIterator: TaskTreeIterator, items: number[]): number => {
+    const availableItems = treeIterator.getAvailable(items);
+	const nextItem = availableItems[0];
+	return nextItem;
+}
+
+function prioritised(treeIterator: TaskTreeIterator, items: number[]) {
+    const availableItems = treeIterator.getAvailable(items);
+	const totalPriority = availableItems.map(id=>treeIterator.readItem(id)).map(item=>item.priority).reduce((acc,curr)=>acc+curr, 0);
+	let totalVisits = availableItems.map(treeIterator.getVisitCount).reduce((acc,curr)=>acc+curr, 0)
+	const maxPossibleVisits = (Math.floor(totalVisits/availableItems.length)+1)*availableItems.length
+	totalVisits = maxPossibleVisits;
+	let bestScore = 0;
+	let nextItem;
+	for (const id of availableItems) {
+		const item = treeIterator.readItem(id);
+		const prioQuot = item.priority/totalPriority;
+		const visitQuot = totalVisits === 0? 0 : treeIterator.getVisitCount(id)/totalVisits;
+		const score = prioQuot - visitQuot;
+		
+		if ((bestScore === 0) || (score > bestScore)){
+			bestScore = score;
+			nextItem = item;
+		}
+	};
+	
+	if (nextItem) {
+        treeIterator.visit(nextItem.id);
+		return nextItem;
+	}
+}
+
+
+export class TaskTreeIterator {
+    visitCache = new Map<number, CacheEntry>();
+    constructor(private taskStore: TaskStore) {
+        this.setupCache();
+    }
+
+    private setupCache() {
+        
+        const worker = (parent: number, items: number[]) => {
+            const algo = simpleOrder;
+            const cacheEntry: CacheEntry = {
+                iterator: algo,
+                visitState: VisitState.OPEN,
+                visits: 0,
+                filtered: false,
+            }
+            this.visitCache.set(parent, cacheEntry);
+            items.forEach(itemId => {
+                worker(itemId, this.taskStore.nodes[itemId].children)
+            });
+        }
+        const topLevel = this.taskStore.topLevel;
+        worker(0, topLevel)
+    }
+    
+    /**
+     * Return the next child of the requested item
+     * @param id Optionally, the id of the child to search, if not provided, top level assumed
+     */
+    nextChild(id?: number) {
+        const searchItem = id ? id : 0;
+        const childItems = searchItem === 0 ? this.taskStore.topLevel : this.taskStore.nodes[searchItem].children;
+        return this.visitCache.get(searchItem)?.iterator(this, childItems);
+    }
+
+    setDone(id: number) {
+        const entry = this.visitCache.get(id);
+        if(entry) {
+            entry.visitState = VisitState.DONE
+        }
+    }
+
+    isDone(id: number) {
+        const entry = this.visitCache.get(id);
+        if(entry) {
+            return entry.visitState === VisitState.DONE
+        }
+        return false;
+    }
+
+    setFiltered(id: number, value = true) {
+        const entry = this.visitCache.get(id);
+        if(entry) {
+            console.log(id, value)
+            entry.filtered = value;
+        }
+    }
+
+    overTakeLimit(id: number) {
+        const item = this.readItem(id);
+        const children = item.children;
+        if (item.take === Take.ONE) {
+            const childrenDoneThisIteration = children.map(child => this.visitCache.get(child)).filter(cacheEntry => cacheEntry?.visitState === VisitState.ITERATION_DONE || cacheEntry?.visitState === VisitState.ITERATION_CHILD_DONE);
+            // console.log(item.id, 'ONE', children, childrenDoneThisIteration.length)
+            return childrenDoneThisIteration.length > 0;
+        }
+        return this.getAvailable(children).length === 0;
+    }
+
+    setIterationDone(id: number) {
+        const entry = this.visitCache.get(id);
+        if(entry) {
+            entry.visitState = VisitState.ITERATION_DONE
+        }
+    }
+
+    setIterationChildDone(id: number) {
+        const entry = this.visitCache.get(id);
+        if(entry) {
+            entry.visitState = VisitState.ITERATION_CHILD_DONE
+        }
+    }
+
+    visit(id: number) {
+        const entry = this.visitCache.get(id);
+        if(entry) {
+            entry.visits++;
+        }
+    }
+
+    getVisitCount(id: number) {
+        const entry = this.visitCache.get(id);
+        if(entry) {
+            return entry.visits;
+        }
+        return 0;
+    }
+
+    /**
+     * Empty (EMPTY)
+     * Has remaining unvisited items (OPEN)
+     * some in ITERATION_COMPLETE (ITERATION_COMPLETE)
+     * Has no remaining unvisited items AND no open recurring items (COMPLETE)
+     * 
+     * some items in open
+     * empty - nothing in it - probably include filtered only items in this
+     * some items in iteration_child_done
+     * some items in iteration_done
+     * some items in done
+     * @param items 
+     * @returns 
+     */
+    evaluateItemStatus(id: number) {
+        const items = this.readChildren(id);
+        if(!items.length) {
+            return CollectionState.EMPTY;
+        }
+        if(this.getAvailable(items).length>0){
+            return CollectionState.OPEN;
+        }
+        if(this.isComplete(items)) {
+            return CollectionState.COMPLETE;
+        }
+        if(this.someIterationChildDone(items)) {
+            return CollectionState.ITERATION_CHILD_DONE;
+        }
+        return CollectionState.ITERATION_COMPLETE;
+    
+    }
+
+    /**
+     * Get items that are not done, iteration done or filtered.. so open
+     * @param ids 
+     */
+    getAvailable(ids: number[]): number[] {
+        // return ids.map(id=>this.visitCache.get(id)).filter(item=>item?.visitState===VisitState.OPEN)
+        return ids.filter(id => this.visitCache.get(id)?.visitState === VisitState.OPEN)
+    }
+
+    /**
+     * Items with visit count 0
+     * @param ids 
+     */
+    getUnvisited(ids: number[]) {
+        return ids.map(id=>this.visitCache.get(id)).filter(item=>item?.visits===0)
+    }
+
+    /**
+     * Return an item for the provided id
+     * @param id 
+     */
+    readItem(id: number) {
+        return this.taskStore.nodes[id];
+    }
+    readItems(ids: number[]){
+        return ids.map((id)=> this.taskStore.nodes[id]);
+    }
+    readChildren(id = 0) {
+        if (id === 0) {
+            return this.taskStore.topLevel;
+        } else {
+            return this.taskStore.nodes[id].children;
+        }
+    }
+
+    /**
+     * Some are iteration_done and the rest are either done or filtered
+     * @param ids 
+     */
+    someIterationDone(ids: number[]): boolean {
+        const items = ids.map(id=>this.visitCache.get(id));
+        return items.some((item)=>(item?.visitState === VisitState.ITERATION_DONE));
+    }
+    someIterationChildDone(ids: number[]): boolean {
+        const items = ids.map(id=>this.visitCache.get(id));
+        return items.some((item)=>(item?.visitState === VisitState.ITERATION_CHILD_DONE));
+    }
+
+    /**
+     * Items are either done or filtered, never open or iteration_done
+     * @param ids 
+     */
+    isComplete(ids: number[]): boolean {
+        return ids.map(id=>this.visitCache.get(id)).every((item)=>(item?.visitState === VisitState.DONE || item?.filtered))
+    }
+
+    /** 
+     * Set all iteration_done items to open
+     */
+    nextIteration() {
+        this.visitCache.forEach((cacheEntry) => {
+            if(cacheEntry.visitState === VisitState.ITERATION_CHILD_DONE) {
+                cacheEntry.visitState = VisitState.OPEN;
+            }
+        })
+    }
+
+    resetIterationChildDoneCounts(parentId=0) {
+        const ids = this.readChildren(parentId);
+        ids.forEach(id => {
+            const item = this.visitCache.get(id);
+            if(item?.visitState === VisitState.ITERATION_CHILD_DONE) {
+                item.visitState = VisitState.OPEN;
+            }
+        })
+    }
+    resetIterationDoneCounts(parentId=0) {
+        const ids = this.readChildren(parentId);
+        ids.forEach(id => {
+            const item = this.visitCache.get(id);
+            if(item?.visitState === VisitState.ITERATION_DONE) {
+                item.visitState = VisitState.OPEN;
+            }
+        })
+    }
+}
